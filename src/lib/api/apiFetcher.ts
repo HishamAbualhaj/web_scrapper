9cefcd437c3ac6832fa791ed42b5517e5b700883
Hisham Abualhaj <hishamecommerce@gmail.com>
2026-01-26 14:23:52 +0200

    helper function to handle fetch api requests and responses

---
+++ src/lib/api/apiFetcher.ts
@@ -0,0 +1,42 @@
+// lib/apiFetcher.ts
+import { ApiError, ApiRequest } from "@/types/api";
+
+export async function apiFetcher<TResponse, TBody = unknown>(
+  request: ApiRequest<TBody>,
+): Promise<TResponse> {
+  const { apiUrl, method, body, headers, signal } = request;
+
+  const res = await fetch(apiUrl, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+    body: body ? JSON.stringify(body) : undefined,
+    signal,
+  });
+
+  let data: any = null;
+
+  try {
+    data = await res.json();
+  } catch {
+    const error: ApiError = {
+      message: data?.message || "Request failed",
+      status: res.status,
+      errors: data?.errors,
+    };
+    throw error;
+  }
+
+  if (!res.ok) {
+    const error: ApiError = {
+      message: data?.message || "Request failed",
+      status: res.status,
+      errors: data?.errors,
+    };
+    throw error;
+  }
+
+  return data as TResponse;
+}
