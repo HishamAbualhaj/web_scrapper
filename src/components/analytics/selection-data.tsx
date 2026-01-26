58ef4713e936be854a227a61c7a57d199e6953a6
Hisham Abualhaj <hishamecommerce@gmail.com>
2026-01-26 15:32:20 +0200

    Selection data for stores and products for analytics chart price

--- src/components/analytics/selection-data.tsx
+++ src/components/analytics/selection-data.tsx
@@ -1,20 +1,72 @@
 "use client";
 import SelectProduct from "@/components/analytics/select-product";
 import SelectStore from "@/components/analytics/select-store";
-import { stores } from "@/data/stores";
+import { useApiMutation } from "@/hooks/useApiMutation";
+import { useApiQuery } from "@/hooks/useApiQuery";
+import {
+  getProductsResponse,
+  ProductAnalytics,
+  Store,
+} from "@/types/api/response";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useQueryState } from "nuqs";
+import { useEffect } from "react";
 const SelectionData = () => {
   const [store] = useQueryState("store");
-  const storeSelected = stores.find((s) => s.id === store);
+
+  const { data: dataProducts, isPending } = useApiQuery<getProductsResponse[]>(
+    ["products_selection", store],
+    {
+      apiUrl: "/api/getProducts",
+      method: "POST",
+      body: {
+        store_id: store,
+      },
+    },
+    { enabled: !!store },
+  );
+
+  const { data: dataStores } = useApiQuery<Store[]>(["stores"], {
+    apiUrl: "/api/analytics/stores",
+    method: "GET",
+  });
+
+  const {
+    mutate,
+    isPending: isPendingProductPrice,
+    data,
+  } = useApiMutation<ProductAnalytics, { productId: string }>();
+
   return (
     <div className="flex max-lg:flex-col gap-6">
-      <SelectStore withUrlState={true} stores={stores} value={store ?? ""} />
+      <SelectStore
+        withUrlState={true}
+        stores={
+          dataStores?.map(
+            ({ store_id, store_name, products_count, created_at }) => ({
+              store_id,
+              store_name,
+              products_count,
+              created_at,
+            }),
+          ) ?? []
+        }
+        value={store ?? ""}
+      />
 
       {store && (
         <SelectProduct
-          products={storeSelected?.products ?? []}
+          isPending={isPending}
+          products={dataProducts ?? []}
           onSelect={(productId) => {
-            console.log("Selected product:", productId);
+            if (isPendingProductPrice) return;
+            mutate({
+              apiUrl: "/api/getProductPrice",
+              method: "POST",
+              body: {
+                productId,
+              },
+            });
           }}
         />
       )}
